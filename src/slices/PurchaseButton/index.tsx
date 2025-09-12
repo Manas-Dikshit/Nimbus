e25aa179c36b95e5e43365ce4531b3ae7e7a6410
Santhosh VS <santhosh02vs@gmail.com>
2025-09-12 14:07:08 +0530

    feat(checkout): implement purchase button slice with animated UI and Stripe checkout redirect

--- src/slices/PurchaseButton/index.tsx
+++ src/slices/PurchaseButton/index.tsx
@@ -13,6 +13,7 @@ import clsx from "clsx";
 import { LuChevronRight, LuLoader } from "react-icons/lu";
 import gsap from "gsap";
 import { useGSAP } from "@gsap/react";
+import { checkout } from "@/checkout";
 
 gsap.registerPlugin(useGSAP);
 
@@ -32,9 +33,7 @@ const PurchaseButton: FC<PurchaseButtonProps> = ({ slice }) => {
 
   const handlePurchaseClick = async () => {
     setIsPressed(true);
-
-    // TODO: add checkout logic later
-    await new Promise((resolve) => setTimeout(resolve, 2000));
+    await checkout();
     setIsPressed(false);
   };
 
